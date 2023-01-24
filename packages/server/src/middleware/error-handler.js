e9eba5358b463dce284d7493cf876ac8a9265bf8
Phil Willis <philopian@gmail.com>
2023-01-23 22:22:02 -0800

    middleware error handler

---
+++ packages/server/src/middleware/error-handler.js
@@ -0,0 +1,7 @@
+export const errorBadJson = (err, req, res, next) => {
+  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+    console.error(err)
+    return res.status(400).send({ status: 404, message: err.message }) // Bad request
+  }
+  next()
+}
